dd926bb23cd04fe9934b7400d5ce4c2791cee564
muhammad-shayan <shayanksa@gmail.com>
2023-11-19 13:59:55 +0500

    stripe fix

--- trpc/index.ts
+++ trpc/index.ts
@@ -147,7 +147,7 @@ export const appRouter = router({
 
   createStripeSession: privateProcedure.mutation(async ({ ctx }) => {
     const { userId } = ctx;
-    const billingUrl = absoluteUrl("dashboard/billing");
+    const billingUrl = absoluteUrl("/dashboard/billing");
 
     if (!userId) throw new TRPCError({ code: "UNAUTHORIZED" });
     const dbUser = await db.user.findFirst({
